6cbcf31170b5356c2c4839bcbd65b68f5f59f221
Antony Yu <antonyyu@gmail.com>
2022-09-27 21:55:24 -0700

    Tip Calculator Done

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.get('/greeting/:name',(req,res) => {
     res.status(200).json({ msg: `Hello, ${req.params.name}!`})
 })
 
+// Tip Calculator
+app.get('/tip/:total/:tipPercentage',(req,res) => {
+    const tipAmount= parseInt(req.params.total) * parseInt(req.params.tipPercentage)/100
+    res.status(200).json({ msg: `Tip Amount: $${tipAmount}`})
+})
 
 
-app.listen(3000, () => {
-    console.log('Listening on Port 3000')
+app.listen(3001, () => {
+    console.log('Listening on Port 3001')
 })
